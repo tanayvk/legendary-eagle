a41f6ed95be8bebc58e217b591cea259473a6e94
Tanay Karnik <tanayvk@gmail.com>
2021-02-04 23:18:04 +0530

    Implemented create workspace function

--- functions/create.js
+++ functions/create.js
@@ -1,8 +1,8 @@
 const firebase = require("firebase-admin");
-console.log(process.version);
+const CryptoJS = require("crypto-js");
+const Base64 = require("crypto-js/enc-base64");
 
 var serviceAccount = JSON.parse( process.env.FIREBASE_KEY );
-
 if ( firebase.apps.length == 0 )
 {
     firebase.initializeApp({
@@ -12,18 +12,49 @@ if ( firebase.apps.length == 0 )
 }
 else
     firebase.app();
-
 var database = firebase.database();
 
-exports.handler = async function(event, context) {
-    message = "success";
+function generateRandomLetter()
+{
+    let letterIndex = Math.floor(Math.random() * 26);
+    return String.fromCharCode("a".charCodeAt(0) + letterIndex);
+}
 
-    data = [];
-    await database.ref('hello').get().then(x => { console.log(x.val()) }).catch(x=>{console.log(x);});
+function generateWorkspaceName()
+{
+    let letters = [];
+    for (var i = 0; i < 10; i++)
+    {
+        letters.push(generateRandomLetter());
+    }
+    return letters.join("");
+}
+
+exports.handler = async function(event, context) {
+    let workspaceName = generateWorkspaceName();
+    let password = generateWorkspaceName();
+
+    // compute the double hash to store in the database
+    // Note: password is hashed once on the frontend and once on the backend
+    let hash = Base64.stringify(CryptoJS.SHA256(password));
+    hash = Base64.stringify(CryptoJS.SHA256(hash));
+
+    var workspaceRef = database.ref('workspaces/' + workspaceName);
+
+    workspaceRef.set({
+        'name': workspaceName,
+        'password': hash,
+    }).then(() => console.log("done"));
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: message, data: data })
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            'name': workspaceName,
+            'password': password,
+        })
     };
 }
 
