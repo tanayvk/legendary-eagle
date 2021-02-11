62dc29fae02fb430ed9bc675051cdb884c06430c
amritj7 <amritansh7757@gmail.com>
2021-02-11 22:14:26 +0530

    create loadingbutton

--- src/components/CreateWorkspace.js
+++ src/components/CreateWorkspace.js
@@ -1,13 +1,11 @@
 import React from "react";
 import axios from "axios";
 import { API_URL } from "../utils/constants";
-import {
-    Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CreateForm(props)
 {
-    console.log(props.visibility);
+   // console.log(props.visibility);
     return <div>
         <form className={"space-y-3 mb-3 " + (props.visibility ? '' : 'hidden ')}>
             <input type="text" placeholder="Workspace Handle" className="py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500 w-full" value={props.workspaceName} />
@@ -33,10 +31,13 @@ class CreateWorkspace extends React.Component
             return;
         this.setState({
             formLoading: true,
+        
         });
         axios.get(API_URL + 'create')
             .then((response) => {
+                console.log(response.data.name);
                 this.setState({
+
                     workspaceName: response.data.name,
                     workspacePassword: response.data.password,
                     formLoading: false,
@@ -55,14 +56,32 @@ class CreateWorkspace extends React.Component
                     workspacePassword={this.state.workspacePassword}
                 />
     }
-
-    renderCreateButton() {
-        return (<button className={"w-full text-white p-3 rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 font-semibold text-lg focus:border focus:border-2 "} onClick={this.handleCreate}>
-                { this.state.formLoading ? "Just a sec..." : "Create New Workspace" }
-            </button>);
+    renderWorkspaceButton(){
+        return( <button className={"w-full text-white p-3 rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 font-semibold text-lg focus:border focus:border-2 "} onClick={this.handleCreate}>
+            Create new Workspace
+        </button>
+        );
     }
-
+    renderCreateLoading(){
+        return (  <div class="relative flex justify-center items-center ">
+        <div class="inline-block animate-spin ease duration-50 w-5 h-5  bg-gradient-to-r from-pink-500 to-yellow-500 mx-2"></div>
+        <div class="inline-block animate-spin ease duration-50 w-5 h-5  bg-gradient-to-r from-pink-500 to-yellow-500 mx-2"></div>
+        <div class="inline-block animate-spin ease duration-50 w-5 h-5 bg-gradient-to-r from-pink-500 to-yellow-500 mx-2"></div>
+        <div class="inline-block animate-spin ease duration-50 w-5 h-5 bg-gradient-to-r from-pink-500 to-yellow-500 mx-2"></div>
+    </div>
+         
+        );
+    }
+    renderCreateButton() {
+        
+        return ( <div>
+            { this.state.formLoading ?  this.renderCreateLoading()   : this.renderWorkspaceButton() }
+        </div> 
+        );
+    }
+   
     renderAccessButton() {
+       
         return <Link to="/workspace"><p className={"w-full text-white text-center p-3 rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 font-semibold text-lg focus:border focus:border-2 "}>Access Workspace</p></Link>;
     }
 
