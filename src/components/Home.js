3f5d8f31ad97950e43a3bdc215e5c229e8c519cc
amritj7 <amritansh7757@gmail.com>
2021-02-15 14:07:05 +0530

    update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,29 @@
 import CreateWorkspace from "./CreateWorkspace.js";
 import { useHistory } from "react-router-dom";
+import HomeDesign from "./HomeDesign";
 
 function Home(props) {
   const history = useHistory();
   return (
-    <div class="h-screen overflow-hidden flex items-center justify-center">
-      <div className="py-20 px-8 md:px-10 lg:px-16 xl:px-20 h-screen w-screen flex flex-col-reverse md:flex-row items-center justify-center ">
-        <div className="space-y-8 container mx-auto flex flex-col items-center">
-          <div>
-            {/* <h1 className="text-5xl text-white font-bold">Legendary Eagle</h1> */}
+    <div class="h-screen overflow-hidden bg-indigo-300 items-center justify-center">
+      <div class="grid grid-cols-5">
+        <div class="col-span-3">
+          <HomeDesign />
+        </div>
+        <div className="py-20 px-20 md:px-10 lg:px-16 w-3/4 mt-20 col-span-2 xl:px-2   items-center justify-center ">
+          <div className="space-y-8 container items-center">
+            <div>
+              {/* <h1 className="text-5xl text-white font-bold">Legendary Eagle</h1> */}
+            </div>
+            <div className="shadow-lg  w-full p-8  bg-white rounded-xl">
+              <CreateWorkspace history={history} />
+            </div>
+            <p className="text-center text-sm my-4">
+              <span className="font-semibold text-white text-center w-full">
+                &copy; Trio
+              </span>
+            </p>
           </div>
-          <div className="shadow-lg xl:w-1/3 w-full p-8 flex flex-col bg-white rounded-xl">
-            <CreateWorkspace history={history} />
-          </div>
-          <p className="text-center text-sm my-4">
-            <span className="font-semibold text-white text-center w-full">
-              &copy; Trio
-            </span>
-          </p>
         </div>
       </div>
     </div>
