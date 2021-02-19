e77fedfeb0d89da93e098fd5233f9bd2bdc804aa
Tanay Karnik <tanayvk@gmail.com>
2021-02-19 13:08:54 +0530

    Make API URL relative

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "https://legendary-eagle.netlify.app/.netlify/functions/";
+const API_URL = "/.netlify/functions/";
 //const API_URL = "http://localhost:8888/.netlify/functions/";
 
 module.exports = {
